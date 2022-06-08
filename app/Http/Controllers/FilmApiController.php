3655174bae6cd5dba217289399ae352a21481335
unknown <ahsan.ghori@laksol.com>
2022-06-08 23:18:16 +0500

    change param

--- app/Http/Controllers/FilmApiController.php
+++ app/Http/Controllers/FilmApiController.php
@@ -98,9 +98,9 @@ class FilmApiController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($slug)
     {
-        return Film::find($id);
+        return Film::where("name",$slug)->first();
     }
 
     /**
